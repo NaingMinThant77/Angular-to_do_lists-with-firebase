b8d02cca6e440d703a8126f8ba912348f5ab3ed7
Marco <naingminthant07@gmail.com>
2023-10-10 06:35:33 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'toDo_ListsApp';
+export class AppComponent implements OnInit {
+  taskArray: any[] = [];
+  taskName: string = '';
+  apiUrl = 'https://todoapp-e3caf-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchTodos();
+  }
+
+  onSubmit() {
+    if (this.taskName.trim() !== '') {
+      this.taskArray.push({
+        taskName: this.taskName,
+        isCompleted: false,
+      });
+      this.taskName = '';
+      this.saveTaskToFirebase();
+    }
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1);
+    this.saveTaskToFirebase();
+  }
+
+  fetchTodos() {
+    this.http.get(this.apiUrl).subscribe((data: any) => {
+      if (data) {
+        this.taskArray = Object.keys(data).map((key) => ({
+          taskName: data[key].title,
+          isCompleted: data[key].completed,
+        }));
+      } else {
+        this.taskArray = [];
+      }
+    });
+  }
+
+  saveTaskToFirebase() {
+    const tasksData = this.taskArray.map((task) => ({
+      title: task.taskName,
+      completed: task.isCompleted,
+    }));
+
+    this.http.put(this.apiUrl, tasksData).subscribe(() => {
+      console.log('Data saved to Firebase');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule], // Add FormsModule here
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
